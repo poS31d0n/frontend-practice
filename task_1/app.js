a86eae2d8783ae9081efbecb2d8d97fdf7a4a3cf
pos31d0n <pos31d0n@192.168.1.5>
2023-02-22 17:26:27 +0300

    task_1_new

--- task_1/app.js
+++ task_1/app.js
@@ -1,11 +1,11 @@
 let x = 1;
-let y = 8;
+let y = 7;
 let string = "#";
 if (x < 1 || x >= y)
     console.log("Bad arguments =(");
 else
 {
-    while (x != y)
+    while (x <= y)
     {
         console.log(string);
         string += '#';
