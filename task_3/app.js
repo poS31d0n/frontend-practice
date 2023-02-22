7833dac79a129850d90c544c2eed6e8f9dad9f18
pos31d0n <pos31d0n@192.168.1.5>
2023-02-22 19:40:32 +0300

    task_3

---
+++ task_3/app.js
@@ -0,0 +1,26 @@
+let x = 8;
+let y = 8;
+if (x < 1 || y < 1)
+    console.log("Bad arguments =(");
+else
+{
+    for (let i = x;i != 0; i--)
+    {
+        let string = '';
+        for (let j = y; j != 0; j--)
+        {
+            if (j % 2 === y % 2 )
+            {
+                if (i % 2 != x % 2)
+                {
+                    string += ' ';
+                    j--;
+                }
+                string += '#';
+            }
+            else
+                string += ' ';
+        }
+        console.log(string);
+    }
+}
