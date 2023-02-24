331f38618d88bb63a4014849f919d860f1935064
pos31d0n <pos31d0n@192.168.1.5>
2023-02-24 19:34:25 +0300

    new_task_1

--- week-1/task_1/app.js
+++ week-1/task_1/app.js
@@ -1,3 +1,5 @@
+////////////////////////////////////////// N_1
+
 function triangle(x , y)
 {
     let string = "#";
@@ -11,7 +13,51 @@ function triangle(x , y)
             string += '#';
             x++;
         }
+        console.log('\n');
     }
 }
 
 triangle(1, 7);
+
+////////////////////////////////////////// N_2
+
+function triangle_2(x , y)
+{
+    let string = '';
+    let n = '';
+    if (x < 1 || x >= y)
+        console.log("Bad arguments =(");
+    else
+    {
+        for (let i = 0; i != y - x + 1; i++)
+        {
+            string += n + '#\n';
+            n += '#';
+        }
+        return string;
+    }
+}
+
+console.log(triangle_2(1, 7));
+
+////////////////////////////////////////// N_3
+
+function triangle_3(x , y)
+{
+    let string = '';
+    if (x < 1 || x >= y)
+        console.log("Bad arguments =(");
+    else
+    {
+        while (x <= y)
+        {
+            for (let i = 0; i != x; i++)
+                string += '#';
+            string += '\n';
+            x++;
+        }
+        return string;
+    }
+}
+
+console.log(triangle_3(3, 10));
