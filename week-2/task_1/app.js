f860e6a85ee7c9a4c7a29d775f26b5f315abaea7
pos31d0n <pos31d0n@192.168.1.8>
2023-03-10 16:38:27 +0300

    333

--- week-2/task_1/app.js
+++ week-2/task_1/app.js
@@ -1,1 +1,14 @@
-import axios from 'axios'
+
+document.querySelector('#component_1').onclick = function() {
+    document.getElementById("menu_1").style.display = "none";
+    document.querySelector('#component_2').style.background = "#FFFFFF";
+    document.getElementById("menu_2").style.display = "grid";
+    document.querySelector('#component_1').style.background = "grey";
+}
+document.querySelector('#component_2').onclick = function() {
+    document.getElementById("menu_2").style.display = "none";
+    document.querySelector('#component_1').style.background = "#FFFFFF";
+    document.getElementById("menu_1").style.display = "flex";
+    document.querySelector('#component_2').style.background = "grey";
+
+}
